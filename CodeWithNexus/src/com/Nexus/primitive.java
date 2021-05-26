055b2e851ad1f808cbc0d8fc23b11dc47de02ad1
ishiikaa <ishiikaa>
2021-05-26 13:23:33 +0530

    rename

---
+++ CodeWithNexus/src/com/Nexus/primitive.java
@@ -0,0 +1,14 @@
+package com.Nexus;
+
+ public class primitive {
+
+    public static void main(String[] args) {
+	// write your code here
+       byte byteValue = 10;
+       short shortValue = 20;
+       int intValue = 50;
+
+       long longTotal = 50000L + 10L * (byteValue + shortValue + intValue);
+        System.out.println(longTotal);
+            }
+}
