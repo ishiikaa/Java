a6d787771240e92939e3b081df702972371a71e5
ishika <78346642+ishiikaa@users.noreply.github.com>
2021-05-28 15:24:40 +0530

    Add files via upload

---
+++ char.java
@@ -0,0 +1,20 @@
+package com.Nexus;
+public class char {
+
+    public static void main(String[] args) {
+	// write your code here
+
+                char myChar = 'D';
+                char myUnicodeChar = '\u0044';
+                System.out.println(myChar);
+                System.out.println(myUnicodeChar);
+                char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
+
+            }
+}
